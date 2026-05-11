b50f2196cb331cbfbf4f9b560d3cef413c734088
527cbowman-sys <527cbowman@frhsd.com>
2026-05-11 11:56:15 -0400

    set up

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var drinkData = {
-    cafeAmericano: {
-        name: "Cafe Americano",
-        type: "Coffee",
-        temp: "Hot",
-        calories: 5,
-    },
+// var drinkData = {
+//     cafeAmericano: {
+//         name: "Cafe Americano",
+//         type: "Coffee",
+//         temp: "Hot",
+//         calories: 5,
+//     },
 
-},
+// },
 
 
 
-let drinkData = document.getElementById('drinkData');
+// let drinkData = document.getElementById('drinkData');
 
-stateList.onchange = function () {
-    let selected = this.value;
-    document.getElementById('info-name').textContent = stateData[selected].name;
-    document.getElementById('info-capitol').textContent = stateData[selected].capitol;
-    document.getElementById('info-population').textContent = stateData[selected].pop;
-    document.getElementById('info-statehood').src = stateData[selected].statehood;
-    document.getElementById('info-abbreviation').textContent = stateData[selected].abbr;
-    document.getElementById('info-mammal').textContent = stateData[selected].mammal;
-    document.getElementById('info-bird').textContent = stateData[selected].bird;
-    document.getElementById('info-flower').textContent = stateData[selected].flower;
-    document.getElementById('info-tree').textContent = stateData[selected].tree;
-}
+// stateList.onchange = function () {
+//     let selected = this.value;
+//     document.getElementById('info-name').textContent = stateData[selected].name;
+//     document.getElementById('info-capitol').textContent = stateData[selected].capitol;
+//     document.getElementById('info-population').textContent = stateData[selected].pop;
+//     document.getElementById('info-statehood').src = stateData[selected].statehood;
+//     document.getElementById('info-abbreviation').textContent = stateData[selected].abbr;
+//     document.getElementById('info-mammal').textContent = stateData[selected].mammal;
+//     document.getElementById('info-bird').textContent = stateData[selected].bird;
+//     document.getElementById('info-flower').textContent = stateData[selected].flower;
+//     document.getElementById('info-tree').textContent = stateData[selected].tree;
+// }
